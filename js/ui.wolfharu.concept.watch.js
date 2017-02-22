c98e52da62d50e7b1cc085c60b05d0bfb07e0097
webdi21 <webdi21@gmail.com>
2017-02-22 11:22:00 +0900

    시계에 대한 CSS와 JS 파일

--- js/ui.wolfharu.concept.watch.js
+++ js/ui.wolfharu.concept.watch.js
@@ -1,2 +1,30 @@
 $(function() {
+	
+	function fnWolfharuWatch() {
+		var currentTime = new Date();
+		var now = {};
+		var angel = {};
+
+		now.hours = currentTime.getHours();
+		now.minutes = currentTime.getMinutes();
+		now.seconds = currentTime.getSeconds();
+
+		if ( now.hours > 12 ) {
+			now.hours -= 12;
+		}
+
+		console.log(now.hours, now.minutes, now.seconds);
+
+		angel.minutes = (360*now.minutes)/60;
+		angel.hours = (360*now.hours)/12 + angel.minutes/60;
+
+		$('#WolfharuWatch strong').css({'transform':'rotate('+angel.hours+'deg)'});
+		$('#WolfharuWatch em').css({'transform':'rotate('+angel.minutes+'deg)'});
+	}
+
+	 fnWolfharuWatch();
+	 setInterval(function() {
+	 	 fnWolfharuWatch();
+	 },1000);
+	
 });
